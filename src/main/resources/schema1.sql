ff7a7c34649a9c0a338552a32065160205f6b030
Bojan <bojan.zolnaj@gmail.com>
2018-09-29 00:13:52 +0200

    Adding INNODB in DB schema.

--- src/main/resources/schema1.sql
+++ src/main/resources/schema1.sql
@@ -1,25 +1,30 @@
 CREATE DATABASE IF NOT EXISTS geodictionary;
 
-CREATE TABLE IF NOT EXISTS geodictionary.languages (
+USE geodictionary;
+
+CREATE TABLE IF NOT EXISTS languages (
                     ID int NOT NULL AUTO_INCREMENT,
                     EnglishName varchar(255) NOT NULL,
                     NativeName varchar(255),
                     IsoCode char(3) UNIQUE,
-                    PRIMARY KEY (ID));
+                    PRIMARY KEY (ID))
+					ENGINE=INNODB;
 					
-CREATE TABLE IF NOT EXISTS geodictionary.terms (
+CREATE TABLE IF NOT EXISTS terms (
                     ID int NOT NULL AUTO_INCREMENT,
                     Term varchar(255) NOT NULL,
                     Meaning text,
                     LanguageID int NOT NULL,
                     PRIMARY KEY (ID),
-                    FOREIGN KEY (LanguageID) REFERENCES Languages(ID));
+                    FOREIGN KEY (LanguageID) REFERENCES Languages(ID))
+					ENGINE=INNODB;
 
-CREATE TABLE IF NOT EXISTS geodictionary.translations (
+CREATE TABLE IF NOT EXISTS translations (
                     ID int NOT NULL AUTO_INCREMENT,
                     Term1ID int NOT NULL,
                     Term2ID int NOT NULL,
                     Priority int NOT NULL,
                     PRIMARY KEY (ID),
                     CONSTRAINT FK_Term1 FOREIGN KEY (Term1ID) REFERENCES Terms(ID),
-                    CONSTRAINT FK_Term2 FOREIGN KEY (Term2ID) REFERENCES Terms(ID));					
+                    CONSTRAINT FK_Term2 FOREIGN KEY (Term2ID) REFERENCES Terms(ID))
+					ENGINE=INNODB;					
