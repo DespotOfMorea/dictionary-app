27a280eda2155d97b0ef1363c1ec00f7049dd3de
Bojan <bojan.zolnaj@gmail.com>
2018-10-05 02:00:11 +0200

    Removing unused imports.

--- src/main/java/com/gzs/main/App.java
+++ src/main/java/com/gzs/main/App.java
@@ -1,8 +1,5 @@
 package com.gzs.main;
 
-import com.gzs.model.Language;
-import com.gzs.model.Term;
-import com.gzs.model.Translation;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
