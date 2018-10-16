8ce9705cc19e9fdccfb7dc4d1a4f1dc1d2516eae
Bojan <bojan.zolnaj@gmail.com>
2018-10-16 21:47:02 +0200

    Missed @Slf4j,while resolving conflict.

--- src/main/java/com/gzs/main/RestMethods.java
+++ src/main/java/com/gzs/main/RestMethods.java
@@ -1,5 +1,7 @@
 package com.gzs.main;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gzs.model.Term;
 
 import javax.ws.rs.*;
@@ -20,16 +22,15 @@ public class RestMethods {
     @GET
     @Path("/post")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.TEXT_HTML)
-    public String getTerm(@QueryParam("term") String termName) {
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getTerm(@QueryParam("term") String termName) throws JsonProcessingException {
         int term1ID = DBMethods.getTermID(termName);
         if (term1ID != 0) {
             int term2ID = DBMethods.getTranslatedTermID(term1ID);
             if (term2ID != 0) {
                 Term term = DBMethods.getTermByID(term2ID);
-                return "<h2>Hello, there is translation for " + termName + " in dictionary.</h2>" +
-                        "<b>" + term.getTerm() + "</b></br>" +
-                        "<i>" + term.getMeaning() + "</i>";
+                ObjectMapper mapper = new ObjectMapper();
+                return mapper.writeValueAsString(term);
             } else {
                 return "<h2>Hello, there is no translation for " + termName + " in dictionary.</h2>";
             }
